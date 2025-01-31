f765d94691a42d79ddfa9ca780e529621bced01e
Abdelrahman <apoeljzzar@gmail.com>
2025-01-31 05:20:14 +0200

    Populate on GetProfile

--- src/Modules/User/user.service.js
+++ src/Modules/User/user.service.js
@@ -2,13 +2,21 @@ import * as dbService from "../../DB/dbService.js";
 import { UserModel } from "../../DB/Models/user.model.js";
 
 export const getProfile = async (req, res, next) => {
-    return res.status(200).json({ success: true, user: req.user });
+    const user = await dbService.findOne({ 
+        model: UserModel, 
+        filter: { _id: req.user._id, isDeleted: false },
+        populate: {
+            path: "viewers.userId",
+            select: "userName email image -_id",
+        },
+    });
+
+    return res.status(200).json({ success: true, user });
 };
 
 
 export const shareProfile = async (req, res, next) => {
     const { profileId } = req.params;
-
     let user = undefined;
 
     if (profileId == req.user._id.toString()) {
